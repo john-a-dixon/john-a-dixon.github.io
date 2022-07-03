c74e27b1a81fc3353958de4c1582f3aae8e62221
John Andrew Dixon <john.a.dixon@outlook.com>
2022-07-03 10:59:40 -0700

    Accidentally added code to the main; will do better next time.

--- resources/js/beerator.js
+++ resources/js/beerator.js
@@ -1,6 +1,15 @@
-document.getElementById('self-image').addEventListener('mousedown', target => {
-    setTimeout(() => {
+const selfImage = document.getElementById('self-image');
+let timer;
+
+selfImage.addEventListener('touchstart', target => {
+    timer = setTimeout(() => {
         document.getElementById('personal-site').style.display = 'none';
         document.getElementById('beerator').style.display = 'block'
     }, 4000);
+
+    console.log(timer);
+});
+
+selfImage.addEventListener('touchend', target => {
+    clearTimeout(timer);
 });
